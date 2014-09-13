a7a0d50211afab8def99620fb6e613a8f32b4f0f
Spencer Elliott <me@elliottsj.com>
2014-09-13 11:23:09 -0400

    Convert PDFs to pngs

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -5,6 +5,10 @@ class DocumentUploader < CarrierWave::Uploader::Base
   # Use Cloudinary integration
   include Cloudinary::CarrierWave
 
+  version :image do
+    process convert: :png
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
