33b961cc3e60056374c416f75fd7c99b52506d25
Jonathan Webb <jwebbed@bell.net>
2014-09-13 11:23:20 -0400

    Added MDParse, well this was anticlimactic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def MDParse(md)
+		renderer = Redcarpet::Render::HTML.new(render_options = {})
+		markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+		return markdown.render(md)
+	end
 end
