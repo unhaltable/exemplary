85f448fa95b01346252fecc6ed31606d4a3a3c62
Nicholas Goh <n.goh@hotmail.com>
2014-09-13 14:10:31 -0400

    cancan and ability shenanigans

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
 
   has_many :documents
   has_many :comments
+  has_and_belongs_to_many :roles
   belongs_to :university
 
+  ##
+  # Return True iff the user has the role specified by the given symbol
+  def has_role?(role_sym)
+  	roles.any? { |r| r.name.underscore.to_sym == role_sym}
+  end
 end
