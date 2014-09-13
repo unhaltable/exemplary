bab988fccd99e95b083ee7fe92b4eef01a09fba5
Jonathan Webb <jwebbed@bell.net>
2014-09-13 13:26:30 -0400

    Fixed the schema

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,5 @@
 class Course < ActiveRecord::Base
 	has_many :documents
 	has_many :notes
+	belongs_to :university
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,18 @@
 
 uni = University.create(name: 'University of Toronto', abbreviation: 'UofT')
 
-User.create(username: 'jwebbed', university: uni)
+me = User.create(username: 'jwebbed', university: uni)
 courses = Course.create([{coursecode: 'STA247', name: "Probability with Computer Applications"},
 	{coursecode: 'CSC108', name: "Introduction to Computer Programming"},
 	{coursecode: 'MAT137', name: "Calculus!"}])
 
+#this is dumb but welp
 uni.courses << courses.first
+uni.courses << courses.second
+uni.courses << courses.third
+
+ezstats = Document.create(name: "That shitty exam from 2012 lol")
+courses.first.documents << ezstats
+me.documents << ezstats
+
+comment = Comment.create(content: "this is the definition of a shitty comment T_T")
