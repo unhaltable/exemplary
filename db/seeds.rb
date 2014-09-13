6f4e9a8b411ddf0b9f2fbc66c62d42be4ed92459
Jonathan Webb <jwebbed@bell.net>
2014-09-13 13:20:24 -0400

    Made each course belong to a university

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+uni = University.create(name: 'University of Toronto', abbreviation: 'UofT')
+
+User.create(username: 'jwebbed', university: uni)
+courses = Course.create([{coursecode: 'STA247', name: "Probability with Computer Applications"},
+	{coursecode: 'CSC108', name: "Introduction to Computer Programming"},
+	{coursecode: 'MAT137', name: "Calculus!"}])
+
+uni.courses << courses.first
