89489839f6fc3139aa16343a20a609fcd76fe3ff
Jonathan Webb <jwebbed@bell.net>
2014-09-14 04:40:47 -0400

    sections page works

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-uni = University.create(name: 'University of Toronto', abbreviation: 'UofT')
-
-me = User.create(username: 'jwebbed', university: uni)
-courses = Course.create([{coursecode: 'STA247', name: "Probability with Computer Applications"},
-	{coursecode: 'CSC108', name: "Introduction to Computer Programming"},
-	{coursecode: 'MAT137', name: "Calculus!"}])
-
-#this is dumb but welp
-uni.courses << courses.first
-uni.courses << courses.second
-uni.courses << courses.third
-
-ezstats = Document.create(name: "That shitty exam from 2012 lol")
-courses.first.documents << ezstats
-me.documents << ezstats
-
-comment = Comment.create(content: "this is the definition of a shitty comment T_T")
-me.comments << comment
+Section.create(document_id: 2)
